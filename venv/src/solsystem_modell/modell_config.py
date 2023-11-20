609e11773a6889d039c5470eb0aacfbcce4b7c39
Tmpecho <TMPEcho@gmail.com>
2023-11-20 19:29:16 +0100

    Updated settings
    Signed-off-by: Tmpecho <TMPEcho@gmail.com>

--- venv/src/solsystem_modell/modell_config.py
+++ venv/src/solsystem_modell/modell_config.py
@@ -4,7 +4,7 @@ SIMULATION_HEIGHT: int = 800
 
 # Constants
 TRAIL_UPDATE_INTERVAL: float = 0.001
-FONT_SIZE: int = 24
+FONT_SIZE: int = 20
 gamma: float = 6.674010551359e-11
 AU: float = 1.495978707e11
 LINE_SKIP_FACTOR: int = 1
@@ -16,5 +16,5 @@ BLACK: tuple = (0, 0, 0)
 # Settings
 DEBUG_MODE: bool = False
 IS_SUN_STATIONARY: bool = True
-zoom: float = 15
+zoom: float = 1.5
 time_acceleration: int = 1000000
